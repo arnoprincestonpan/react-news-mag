332b1befe5e89f8d6a4f00746e79c30a37853d8c
Arno Pan <apan11@my.bcit.ca>
2025-03-19 16:35:55 -0700

    ensure that cards are mostly uniform. Added alt and defaultImage if no image file. Started adding category.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Navbar from './Components/Navbar';
 import NewsBoard from './Components/NewsBoard';
+import { useState } from 'react';
 
 const App = () => {
+  const [category, setCategory] = useState("general");
   return (
     <>
-      <Navbar/>
-      <NewsBoard/>
+      <Navbar
+        setCategory={setCategory}
+      />
+      <NewsBoard
+        category={category}
+      />
     </>
   );
 };
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,12 +1,14 @@
+import defaultImage from '../assets/pexels-scenicspire-358690216-28216688.jpg'
+
 const NewsItem = ( {title, description, src, url} ) => {
   return (
     <div>
       <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 py-2 px-2" style={{maxWidth: "345px"}}>
-        <img src={src} className="card-img-top" alt="..." />
+        <img src={src || defaultImage} style={{height: "200px", objectFit: "cover"}} className="card-img-top" alt={src ? title : "Image Not Available"} />
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
+          <h5 className="card-title">{title.slice(0, 50)}</h5>
           <p className="card-text">
-            {description}
+            {description? description.slice(0,90) : "See the Official Link for Description."}
           </p>
           <a href={url} className="btn btn-primary">
             Read More
