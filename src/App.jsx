38a84cf4a8e1eedb2112062562c8d438951fe67c
Arno Pan <apan11@my.bcit.ca>
2025-03-19 16:07:52 -0700

    bootstrap works, template cleaned, started NewsItem and NewsBoard, Navbar completed. Removed regular JS class and replaced with className

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './Components/Navbar';
+import NewsBoard from './Components/NewsBoard';
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <NewsBoard/>
+    </>
+  );
+};
+
+export default App;
+
