5e7dfa017aa5ec0cdd7d61ee7c4cfbfd8500850e
Arno Pan <apan11@my.bcit.ca>
2025-03-19 16:44:04 -0700

    completed fixed issue with image source src. Used ternary statement instead.

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -4,7 +4,7 @@ const NewsItem = ( {title, description, src, url} ) => {
   return (
     <div>
       <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 py-2 px-2" style={{maxWidth: "345px"}}>
-        <img src={src || defaultImage} style={{height: "200px", objectFit: "cover"}} className="card-img-top" alt={src ? title : "Image Not Available"} />
+        <img src={src ? src : defaultImage} style={{height: "200px", objectFit: "cover"}} className="card-img-top" alt={src ? title : "Image Not Available"} />
         <div className="card-body">
           <h5 className="card-title">{title.slice(0, 50)}</h5>
           <p className="card-text">
