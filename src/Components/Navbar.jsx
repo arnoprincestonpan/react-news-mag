eea42ffd3d3a0f2dfee3196f264f0c2829b1f700
Arno Pan <apan11@my.bcit.ca>
2025-03-19 16:41:14 -0700

    remove navbar items not needed, added category items, passed props for NavBar to setCategory and then use useEffect to reflect category change of useState.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ( {setCategory} ) => {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark"> 
@@ -12,24 +12,22 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active" onClick={() => setCategory("technology")} aria-current="page" href="#">Technology</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
+                <a className="nav-link active" onClick={() => setCategory("business")} aria-current="page" href="#">Business</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                <a className="nav-link active" onClick={() => setCategory("health")} aria-current="page" href="#">Health</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" onClick={() => setCategory("science")} aria-current="page" href="#">Science</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" onClick={() => setCategory("sports")} aria-current="page" href="#">Sports</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" onClick={() => setCategory("entertainment")} aria-current="page" href="#">Entertainment</a>
               </li>
             </ul>
           </div>
